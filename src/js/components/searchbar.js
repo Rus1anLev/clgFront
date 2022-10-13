ce84efab2ba873e3c5c700b3f6b6ffff5d7a0f7f
korchevIvan <unbsold@yandex.ru>
2022-10-13 15:18:35 +0300

    Major fixed

--- src/js/components/searchbar.js
+++ src/js/components/searchbar.js
@@ -2,22 +2,34 @@ import {closeBurgerWindow, closeBurgerMenu} from "./header"
 
 let $header = $('.header'),
     $searchbar = $('.searchbar'),
-    $searchbarBtn = $('.js-searchbar-btn')
+    $searchbarBtn = $('.js-searchbar-btn'),
+    $searchbarInput = $('.searchbar-input')
 
 const closeSearchbar = () => {
     $header.removeClass('is-open')
     $searchbar.removeClass('is-open')
+    $(document).off('mouseup', handlerClickOutSearchbar)
 }
 
 const openSearchbar = () => {
+    $(document).on('mouseup', handlerClickOutSearchbar)
     $header.addClass('is-open')
     $searchbar.addClass('is-open')
+}
+
+function handlerClickOutSearchbar(e) {
+    if (
+        (!$searchbarInput.is(e.target) && $searchbarInput.has(e.target).length === 0) &&
+        (!$searchbarBtn.is(e.target) && $searchbarBtn.has(e.target).length === 0)
+    ) {
+        closeSearchbar()
+    }
+
 }
 
 function handlerSwitcherSearchbar() {
     closeBurgerWindow('all')
     closeBurgerMenu()
-
     if( $searchbar.hasClass('is-open') ) {
         closeSearchbar()
     } else {
